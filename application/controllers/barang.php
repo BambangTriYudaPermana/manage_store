19f6f109e024181cddb3594ede0226c5e1a27f59
bambang.triyuda.permana@gmail.com <bambang.triyuda.permana@gmail.com>
2020-12-30 10:46:16 +0700

    push

--- application/controllers/barang.php
+++ application/controllers/barang.php
@@ -53,7 +53,7 @@ class Barang extends CI_Controller
 		$this->load->view('layout/footer', $this->data);
 	}
 
-	function save($id_barang = null,$status = null){
+	function save(){
 		// cek pelanggan
 		$this->load->model("pelanggan_model");
 		$nama_pelanggan		= $this->input->post('nama_pelanggan');
@@ -86,12 +86,7 @@ class Barang extends CI_Controller
 			'created_date'	=> date('Y-m-d h:i:s')
 		);
 
-		if (isset($status)) {
-			$condition['id_barang']		=	$id_barang;
-			$insert_barang 				= 	$this->barang_model->updateBarang($data_barang,$condition);
-		}else{
-			$insert_barang = $this->barang_model->insertBarang($data_barang);
-		}
+		$insert_barang = $this->barang_model->insertBarang($data_barang);
 
 		
 
@@ -121,6 +116,54 @@ class Barang extends CI_Controller
 		$this->load->view('layout/nav_left', $this->data);
 		$this->load->view('barang/form', $this->data);
 		$this->load->view('layout/footer', $this->data);
+	}
+
+	function save_update($id_barang){
+		// cek pelanggan
+		$this->load->model("pelanggan_model");
+		$nama_pelanggan		= $this->input->post('nama_pelanggan');
+
+		$cek_pelanggan = $this->pelanggan_model->getPelangganByName($nama_pelanggan)->row();
+		if (empty($cek_pelanggan)) {
+			$data_pelanggan = array(
+				'nama_pelanggan' 	=> $nama_pelanggan,
+				'created_date'		=> date('Y-m-d h:i:s'),
+			);
+
+			$this->pelanggan_model->insertPelanggan($data_pelanggan);
+
+			$get_id_pelanggan = $this->pelanggan_model->getOneLast()->row();
+
+			$id_pelanggan = $get_id_pelanggan->id_pelanggan;
+		}else{
+			$id_pelanggan = $cek_pelanggan->id_pelanggan;
+		}
+
+		$harga 				= str_replace(",","",$this->input->post('harga'));
+
+		$data_barang = array(
+			'nama_barang' 	=> $this->input->post('nama_barang'),
+			'qty'			=> $this->input->post('qty'),
+			'harga'			=> $harga,
+			'tgl_booked'	=> $this->input->post('tgl_booked'),
+			'status'		=> $this->input->post('status'),
+			'id_pelanggan'	=> $id_pelanggan,
+			'updated_date'	=> date('Y-m-d h:i:s')
+		);
+
+		$condition['id_barang']		=	$id_barang;
+		$insert_barang 				= 	$this->barang_model->updateBarang($data_barang,$condition);
+
+
+		$status_result = [
+			'title'	=> 'Berhasil!!!',
+			'text'	=> 'Data berhasil Diinputkan..',
+			'status'=> 'success'
+		];
+
+		$res = json_encode($status_result);
+		echo $res;
+		// return $res;
 	}
 
 
